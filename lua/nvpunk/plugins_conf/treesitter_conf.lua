79edde31cc0e96af2508026e870bd4fb4c690a14
Gabriele Musco <gabmus@disroot.org>
2022-09-09 21:46:55 +0200

    disable treesitter if gcc not found

--- lua/nvpunk/plugins_conf/treesitter_conf.lua
+++ lua/nvpunk/plugins_conf/treesitter_conf.lua
@@ -1,41 +1,57 @@
--- local wk = require'which-key'
-
-require'nvim-treesitter.configs'.setup {
-    ensure_installed = 'all',
-    ignore_install = {'phpdoc', 'swift', 'fortran', 'php'},
-    highlight = {
-        enable = true,
-        use_languagetree = true,
-        additional_vim_regex_highlighting = true,
-    },
-    incremental_selection = {
-        enable = false,
-        keymaps = {
-            init_selection = '<leader>ss',
-            node_incremental = '<Up>',
-            scope_incremental = '<S-Up>',
-            node_decremental = '<C-Up>',
+local function setup_treesitter()
+    require'nvim-treesitter.configs'.setup {
+        ensure_installed = 'all',
+        ignore_install = {'phpdoc', 'swift', 'fortran', 'php'},
+        highlight = {
+            enable = true,
+            use_languagetree = true,
+            additional_vim_regex_highlighting = true,
         },
-    },
-    indent = {
-        -- indentation with treesitter isn't great
-        enable = false,
-        disable = {
-            'html', 'scss', 'css', 'yaml', 'python'
-        }
-    },
-    autopairs = {
-        enable = true
-    },
-    autotag = {  -- this is a plugin: nvim-ts-autotag
-        enable = true,
-        filetypes = {
-            'html', 'xml', 'javascript', 'javascriptreact', 'typescriptreact',
-            'svelte', 'vue'
+        incremental_selection = {
+            enable = false,
+            keymaps = {
+                init_selection = '<leader>ss',
+                node_incremental = '<Up>',
+                scope_incremental = '<S-Up>',
+                node_decremental = '<C-Up>',
+            },
+        },
+        indent = {
+            -- indentation with treesitter isn't great
+            enable = false,
+            disable = {
+                'html', 'scss', 'css', 'yaml', 'python'
+            }
+        },
+        autopairs = {
+            enable = true
+        },
+        autotag = {  -- this is a plugin: nvim-ts-autotag
+            enable = true,
+            filetypes = {
+                'html', 'xml', 'javascript', 'javascriptreact', 'typescriptreact',
+                'svelte', 'vue'
+            }
         }
     }
-}
+end
 
--- wk.register {
---     ['<leader>s'] = { name = 'TreeSitter' }
--- }
+require'plenary.job':new({
+    command = '/bin/bash',
+    args = {
+        '-c',
+        '"which gcc"'
+    },
+    on_exit = function(_, res)
+        if res == 0 then
+            setup_treesitter()
+        else
+            vim.notify(
+                'Treesitter is disabled\n' ..
+                'Please install gcc to enable Treesitter',
+                'error',
+                { title = 'nvpunk.plugins.treesitter' }
+            )
+        end
+    end
+})
