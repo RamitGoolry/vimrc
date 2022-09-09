4e6a438db24abd309fdad073e738cb47ff253b36
Gabriele Musco <gabmus@disroot.org>
2022-09-09 13:09:46 +0200

    fix update function; use plenary job

--- lua/nvpunk/punk_funcs.lua
+++ lua/nvpunk/punk_funcs.lua
@@ -1,14 +1,27 @@
 local M = {}
 
-M.nvpunk_install_dir = vim.fn.stdpath 'config'
-
 M.nvpunk_update = function()
-    vim.notify'Updating Nvpunk...'
-    vim.fn.system {
-        'git', 'pull', '-C',
-        M.nvpunk_install_dir
-    }
-    vim.notify'Nvpunk Updated'
+    vim.notify('Updating Nvpunk...', 'info', {
+        title = 'Nvpunk Update'
+    })
+    require'plenary.job':new({
+        command = '/usr/bin/git',
+        args = {
+            '-C', vim.fn.stdpath'config',
+            'pull'
+        },
+        on_exit = function(_, res)
+            if res == 0 then
+                vim.notify('Nvpunk updated', 'info', {
+                    title = 'Nvpunk Update'
+                })
+            else
+                vim.notify('Nvpunk update failed', 'error', {
+                    title = 'Nvpunk Update'
+                })
+            end
+        end
+    }):start()
 end
 
 vim.api.nvim_create_user_command(
