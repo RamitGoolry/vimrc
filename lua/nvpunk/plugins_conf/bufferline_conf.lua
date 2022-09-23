2562881ce363ac8cf28c4320fc0f5a53fdb3e6f2
Gabriele Musco <gabmus@disroot.org>
2022-09-23 15:20:48 +0200

    bufferline ignored files refactoring; switched back to buffer tabs

--- lua/nvpunk/plugins_conf/bufferline_conf.lua
+++ lua/nvpunk/plugins_conf/bufferline_conf.lua
@@ -1,20 +1,26 @@
+local ignored_filetypes = {
+    '',
+    'packer',
+    'TelescopePrompt',
+    'alpha',
+    'NvimTree',
+}
+
+local ignored_filetypes_dict = {}
+for _, f in ipairs(ignored_filetypes) do
+    ignored_filetypes_dict[f] = true
+end
+
 require'bufferline'.setup{
     options = {
-        mode = 'tabs',  -- only show native tabs, alternative: 'buffers'
+        mode = 'buffers',  -- 'buffers' | 'tabs'
         numbers = 'none',
         diagnostics = 'nvim_lsp',
         always_show_bufferline = true,
-        separator_style = 'thin',  -- slant, thick
+        separator_style = 'slant',  -- 'thin' | 'thick' | 'slant'
         custom_filter = function(buf_number, buf_numbers)
-            -- filter out filetypes you don't want to see
-            return not (
-                vim.bo[buf_number].filetype == '' or
-                vim.bo[buf_number].filetype == 'packer' or
-                vim.bo[buf_number].filetype == 'terminal' or
-                vim.bo[buf_number].filetype == 'TelescopePrompt' or
-                vim.bo[buf_number].filetype == 'alpha' or
-                vim.bo[buf_number].filetype == 'NvimTree'
-            )
+            -- return true if filetype is to be ignored
+            return ignored_filetypes_dict[vim.bo[buf_number].filetype] == nil
         end,
     }
 }
