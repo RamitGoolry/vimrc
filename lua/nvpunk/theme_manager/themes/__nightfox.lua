059ae4a7ea614f914fd0ff1f7d30a9f2207b72e6
Gabriele Musco <gabmus@disroot.org>
2022-10-01 10:04:27 +0200

    don't dim inactive in nighfox

--- lua/nvpunk/theme_manager/themes/__nightfox.lua
+++ lua/nvpunk/theme_manager/themes/__nightfox.lua
@@ -9,7 +9,7 @@ return function(style)
                 compile_file_suffix = '_compiled',  -- Compiled file suffix
                 transparent = false, -- Disable setting background
                 terminal_colors = true,  -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-                dim_inactive = true,  -- Non focused panes set to alternative background
+                dim_inactive = false,  -- Non focused panes set to alternative background
                 styles = {              -- Style to be applied to different syntax groups
                     comments = 'italic',    -- Value is any valid attr-list value `:help attr-list`
                     conditionals = 'NONE',
