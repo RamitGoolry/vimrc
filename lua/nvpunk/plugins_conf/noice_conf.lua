8a5028f06513baf7b82e6d6388084c4e8a8ba213
Gabriele Musco <gabmus@disroot.org>
2022-10-05 08:36:59 +0200

    disabled cmp cmdline completion

--- lua/nvpunk/plugins_conf/noice_conf.lua
+++ lua/nvpunk/plugins_conf/noice_conf.lua
@@ -1,33 +1,45 @@
 require'noice'.setup {
-	views = {
-		cmdline_popup = {
-			position = {
-				row = 5,
-				col = '50%',
-			},
-			size = {
-				width = 60,
-				height = 'auto',
-			},
-		},
-		popupmenu = {
-			relative = 'editor',
-			backend = 'nui',
-			position = {
-				row = 8,
-				col = '50%',
-			},
-			size = {
-				width = 60,
-				height = 10,
-			},
-			border = {
-				style = 'rounded',
-				padding = { 0, 1 },
-			},
-			win_options = {
-				winhighlight = { Normal = 'Normal', FloatBorder = 'DiagnosticInfo' },
-			},
-		},
-	},
+    cmdline = {
+        view = 'cmdline_popup',
+        icons = {
+            ['/'] = { icon = ' ', hl_group = 'DiagnosticWarn' },
+            ['?'] = { icon = ' ', hl_group = 'DiagnosticWarn' },
+            [':'] = { icon = ' ', hl_group = 'DiagnosticInfo', firstc = false },
+        },
+    },
+    popupmenu = {
+        enabled = true,
+        ---@type 'nui'|'cmp'
+        backend = 'nui',
+    },
+    views = {
+        cmdline_popup = {
+            position = {
+                row = 5,
+                col = '50%',
+            },
+            size = {
+                width = 60,
+                height = 'auto',
+            },
+        },
+        popupmenu = {
+            relative = 'editor',
+            position = {
+                row = 8,
+                col = '50%',
+            },
+            size = {
+                width = 60,
+                height = 10,
+            },
+            border = {
+                style = 'rounded',
+                padding = { 0, 1 },
+            },
+            win_options = {
+                winhighlight = { Normal = 'Normal', FloatBorder = 'DiagnosticInfo' },
+            },
+        },
+    },
 }
