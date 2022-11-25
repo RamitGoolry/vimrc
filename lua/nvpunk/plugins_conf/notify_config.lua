5b0b2712f72adfe45fe1ec0be3e831f8485771f7
Gabriele Musco <gabmus@disroot.org>
2022-11-25 09:32:35 +0100

    use lua functions for notify keymaps

--- lua/nvpunk/plugins_conf/notify_config.lua
+++ lua/nvpunk/plugins_conf/notify_config.lua
@@ -2,5 +2,13 @@ vim.notify = require'notify'
 
 local km = require'nvpunk.util.keymapper'
 km.wk.register({ ['<leader>n'] = { name = ' Notifications' } }, { mode = 'n' })
-km.nkeymap('<leader>nd', '<cmd>lua require"notify".dismiss()<cr>', 'Dismiss Notifications')
-km.nkeymap('<leader>nl', '<cmd>Telescope notify<cr>', 'List Notifications')
+km.nkeymap(
+    '<leader>nd',
+    function() require'notify'.dismiss() end,
+    'Dismiss Notifications'
+)
+km.nkeymap(
+    '<leader>nl',
+    function() require'telescope'.extensions.notify.notify() end,
+    'List Notifications'
+)
