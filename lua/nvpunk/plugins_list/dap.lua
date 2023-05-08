f5f8e208be8d73388a09e3eee74765a39e2a410b
Ramit Goolry <ramit@wandb.com>
2023-05-08 12:46:51 -0700

    Add a bunch of plugins and keymaps

--- lua/nvpunk/plugins_list/dap.lua
+++ lua/nvpunk/plugins_list/dap.lua
@@ -1,4 +1,11 @@
 return {
     {'mfussenegger/nvim-dap'},
     {'rcarriga/nvim-dap-ui'},
+    {
+      'leoluz/nvim-dap-go',
+      config = function()                   -- TODO: move config to its own file (https://github.com/leoluz/nvim-dap-go)
+        local dap_go = require('dap-go')
+        dap_go.setup()
+      end
+    },
 }
