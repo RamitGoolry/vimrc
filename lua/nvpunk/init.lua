a1b369c185521179410aeffe7ce4495e0d38892f
Ramit Goolry <ramit@wandb.com>
2023-06-08 00:03:02 -0700

    Leverage harpoon to open tabs on start

--- lua/nvpunk/init.lua
+++ lua/nvpunk/init.lua
@@ -15,3 +15,22 @@ vim.cmd([[
 vim.cmd([[
   command! Goiferr call append(line('.'), ['if err != nil {', '   return err, nil', '}'])
 ]])
+
+-- Use Harpoon to load up all files
+-- Step 1 : Check if vim has any commands passed to it
+if vim.fn.argc() == 0 then
+	local harpoon = require('harpoon.mark')
+	local length = harpoon.get_length()
+
+	for i = 1, length do
+		local file_table = harpoon.get_marked_file(i)
+		-- filename, row, col
+		if i == 1 then
+			vim.cmd('e ' .. file_table.filename)
+			vim.fn.cursor(file_table.row, file_table.col)
+		else
+			vim.cmd('tabe ' .. file_table.filename)
+			vim.fn.cursor(file_table.row, file_table.col)
+		end
+	end
+end
