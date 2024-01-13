7ae4f070ffcda9d6f7d74a518124889507c24d1a
Ramit Goolry <ramitgoolry@gmail.com>
2024-01-13 18:55:35 +0530

    remap notifications

--- lua/nvpunk/plugins_conf/notify_config.lua
+++ lua/nvpunk/plugins_conf/notify_config.lua
@@ -1,23 +1,22 @@
-vim.notify = require'notify'
+vim.notify = require('notify')
 local notify = vim.notify
 
 vim.notify = function(msg, ...) -- Suppress multiple different encodings warning
-    if msg:match("warning: multiple different client offset_encodings") then
-        return
-    end
+	if msg:match('warning: multiple different client offset_encodings') then
+		return
+	end
 
-    notify(msg, ...)
+	notify(msg, ...)
 end
 
-local km = require'nvpunk.util.keymapper'
-km.wk.register({ ['<leader>n'] = { name = ' Notifications' } }, { mode = 'n' })
-km.nkeymap(
-    '<leader>nd',
-    function() require'notify'.dismiss() end,
-    'Dismiss Notifications'
+local km = require('nvpunk.util.keymapper')
+km.wk.register(
+	{ ['<leader>N'] = { name = ' Notifications' } },
+	{ mode = 'n' }
 )
-km.nkeymap(
-    '<leader>nl',
-    function() require'telescope'.extensions.notify.notify() end,
-    'List Notifications'
-)
+km.nkeymap('<leader>Nd', function()
+	require('notify').dismiss()
+end, 'Dismiss Notifications')
+km.nkeymap('<leader>Nl', function()
+	require('telescope').extensions.notify.notify()
+end, 'List Notifications')
